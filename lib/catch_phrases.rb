a7356845b8a40139d7f55a6cdb49b3305a844b22
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-08 13:55:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,3 +5,14 @@ end
 def toadstool
   puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
+
+def link 
+  puts link_phrase = "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases 
+  puts mario 
+  puts toadstool
+  puts link
+end
+
